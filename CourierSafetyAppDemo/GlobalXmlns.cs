146ce6d9db238fbf69e809d3fd523be275dde552
n9izyboy <silvanocartinio@gmail.com>
2025-10-06 11:56:42 +0200

    Some changes made to the app

---
+++ CourierSafetyAppDemo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CourierSafetyAppDemo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CourierSafetyAppDemo.Pages")]
